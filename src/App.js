ed50c95f2438f77423a8c7969ee25263b4ca598f
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-01-01 03:13:25 +0900

    :innocent:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import icon from './icon.png';
 import './App.css';
+import { Navbar, Jumbotron, ButtonGroup, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -8,18 +9,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={icon} className="App-logo" alt="logo" />
-          <h1 className="App-title">fornothing.net</h1>
+          <h1 className="App-title">kdxu.fornothing.net</h1>
         </header>
         <h2> About </h2>
           Kyoko Kadowaki. boxing, swimming, and running tiny company.
         <hr/>
         <h2> Links </h2>
-        <ul>
-          <li><a href="https://github.com/kdxu">github</a></li>
-          <li><a href="https://twitter.com/kdxu">twitter</a></li>
-          <li><a href="https://medium.com/@kdxu">medium</a></li>
-          <li><a href="https://keybase.io/kdxu">keybase</a></li>
-        </ul>
+        <ButtonGroup vertical>
+          <Button bsStyle="link" href="https:/github.com/kdxu">github</Button>
+          <Button bsStyle="link" href="https:/twitter.com/kdxu">twitter</Button>
+          <Button bsStyle="link" href="https://medium.com/@kdxu">medium</Button>
+          <Button bsStyle="link" href="https://keybase.io/kdxu">keybase</Button>
+        </ButtonGroup>
         <hr/>
           kyoko.kadowaki [at] gmail.com
       </div>

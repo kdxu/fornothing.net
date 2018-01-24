8fe847bd021540d93210501b2e10c6f6954874aa
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-01-25 03:12:44 +0900

    Add Tweet Button

--- src/Images.js
+++ src/Images.js
@@ -24,6 +24,7 @@ const gifs = [
 
 export default () => (
   <Section title="Gifs" children={
+    <div>
     <Well>
     <Carousel>
     {gifs.map(gif => (
@@ -33,9 +34,15 @@ export default () => (
       <h3>{gif.title}</h3>
       </Carousel.Caption>
       </Carousel.Item>
-  ))}
+    ))}
     </Carousel>
     </Well>
+    <div>
+    <a href="https://twitter.com/intent/tweet?text=限界百合オタク&url=https://kdxu.fornothing.net/gifs" onClick="window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;" rel="nofollow" class="btn btn-social btn-twitter"> <span class="fa fa-twitter"></span>
+    Tweet
+    </a>
+    </div>
+    </div>
   }
   />
 );

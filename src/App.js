e8c905ea8ddd2649ce942088bd2b1a582a9e3995
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-01-25 10:45:02 +0900

    gardening

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import About from "./components/About.js";
 import Header from "./components/Header.js";

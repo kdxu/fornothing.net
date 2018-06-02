c38a2f9bd78db1469279d7762cf3d54a3b7b7c4d
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-06-03 06:50:50 +0900

    Add Pic

--- src/components/Content.js
+++ src/components/Content.js
@@ -16,6 +16,7 @@ import avatar4 from '~/assets/avatars/avatar4.png'
 import avatar5 from '~/assets/avatars/avatar5.png'
 import avatar6 from '~/assets/avatars/avatar6.png'
 import avatar7 from '~/assets/avatars/avatar7.png'
+import avatar8 from '~/assets/avatars/avatar8.png'
 
 const avatars = [
   avatar0,
@@ -25,7 +26,8 @@ const avatars = [
   avatar4,
   avatar5,
   avatar6,
-  avatar7
+  avatar7,
+  avatar8
 ]
 
 export default () => (
